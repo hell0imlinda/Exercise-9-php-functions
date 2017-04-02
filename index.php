80dc673f94c619205d1bc01af6596e7d15bd8ea3
hell0imlinda <linda.ov.600@my.csun.edu>
2017-04-02 16:02:48 -0700

    Exercise-9 | php-functions

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php require_once('functions.php'); ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>Functional Coffee</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+  </head>
+  <body>
+    <main class="container py-4">
+      <h1>Types of Dim Sum</h1>
+      <?php makeCoffee(espresso); ?>
+    </main>
+  </body>
+</html>
