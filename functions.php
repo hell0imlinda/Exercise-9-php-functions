809ae8ca14305d61c81c1e2d965f64133100c2c6
hell0imlinda <linda.ov.600@my.csun.edu>
2017-04-02 17:11:49 -0700

    finished

--- functions.php
+++ functions.php
@@ -3,15 +3,15 @@
 /**
  * Echos coffee incredient.
  *
- * @param Type of coffee
+ * @param Type of Dim Sum
  */
 
-function makeCoffee($type) {
-  if ($type == 'latte') {
-    echo '<p>A latte is a shortened version of the Italian word, caffè latte, which means milk coffee. Lattes contain a shot of espresso and steamed milk.</p>';
-  } else if ($type == 'espresso') {
-    echo '<p>Highly concentrated Italian coffee served in a single standard shot.</p>';
+function makeDimsum($type) {
+  if ($type == 'ha-gow') {
+    echo '<p>Ha gow is a traditional Cantonese dumpling served in dim sum. The dumpling is sometimes called a shrimp bonnet for its pleated shape.</p>';
+  } else if ($type == 'shu-mai') {
+    echo '<p>Shu-mai or siu mai, the Chinese steamed pork and shrimp dumplings, are one of the most popular items at dim sum parlors. But you dont have to go out just to enjoy them, because they&#8217re one of the easiest dumplings to make at home.</p>';
   } else {
-    echo '<p>That&rsquo;s not a real coffee.</p>';
+    echo '<p>That&rsquo;s not a real dimsum dish.</p>';
   }
 }
--- index.php
+++ index.php
@@ -5,13 +5,17 @@
 <html lang="en">
   <head>
     <meta charset="UTF-8">
-    <title>Functional Coffee</title>
+    <title>Types of Dim Sum?</title>
     <link rel="stylesheet" href="css/bootstrap.css">
   </head>
   <body>
     <main class="container py-4">
       <h1>Types of Dim Sum</h1>
-      <?php makeCoffee(espresso); ?>
+      <h2>Ha-Gow</h2>
+      <?php makeDimsum('ha-gow'); ?>
+      <h2>Siu-Mai</h2>
+      <?php makeDimsum('shu-mai'); ?>
+
     </main>
   </body>
 </html>
